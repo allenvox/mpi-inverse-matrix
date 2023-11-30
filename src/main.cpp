1532442a4349d7875473664886f075a3447fda97
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2023-12-01 00:55:41 +0700

    Fix getting main element for equal nrows on procs

--- src/main.cpp
+++ src/main.cpp
@@ -1,61 +1,53 @@
 #include <mpi.h>
 
 #include <cmath>
+#include <exception>
 #include <iomanip>
 #include <iostream>
 
 const std::string prefix = "[inverse matrix] ";
 
-int n, rank, commsize;
+int n, nrows, rank, commsize;
 
 void get_chunk(int *lb, int *ub) {
-  int q = n / commsize;
-  if (n % commsize) q++;
-  int r = commsize * q - n;
-  /* Compute chunk size for the process */
-  int chunk = q;
-  if (rank >= commsize - r) chunk = q - 1;
-  *lb = a;        /* Determine start item for the process */
-  if (rank > 0) { /* Count sum of previous chunks */
-    if (rank <= commsize - r) {
-      *lb += q * rank;
-    } else {
-      *lb += q * (commsize - r) + (q - 1) * (rank - (commsize - r));
-    }
+  int rows_per_process = n / commsize;
+  int remaining_rows = n % commsize;
+  // Determine the lower and upper bounds for each process
+  if (rank < remaining_rows) {
+    *lb = rank * (rows_per_process + 1);
+    *ub = *lb + rows_per_process;
+  } else {
+    *lb = remaining_rows * (rows_per_process + 1) + (rank - remaining_rows) * rows_per_process;
+    *ub = *lb + rows_per_process - 1;
   }
-  *ub = *lb + chunk - 1;
-  std::cout << prefix << "rank " << rank << ": lb = " << *lb << ", ub = " << *ub
-            << '\n';
+  std::cout << prefix << rank << ": lb = " << *lb << ", ub = " << *ub << '\n';
 }
 
-double** get_matrix(double **a) {
-  int lb, ub;
-  get_chunk(&lb, &ub);
-  int nrows = ub - lb + 1;
+double** get_matrix() {
   double **a = new double *[nrows];
-
-  for (int i = 0; i < n / commsize; i++) {
+  for (int i = 0; i < nrows; i++) {
+    a[i] = new double[n];
     for (int j = 0; j < n; j++) {
-      // proc's n-th row = matrix's (i + rank * n / commsize)-th row
       a[i][j] = std::min(n - j, n - i - rank * n / commsize);
     }
   }
+  return a;
 }
 
 void print_matrix(double **a) {
   int k = 0;
   // output only first 6 rows & cols
-  for (int i = 0; i < std::min(n, 6); i++) {
+  for (int i = 0; i < std::min(nrows, 6); i++) {
     // sync all procs before outputting next row
     MPI_Barrier(MPI_COMM_WORLD);
     // each proc outputs his own rows
     if (rank == k) {
-      for (int j = 0; j < std::min(n, 6); j++) {
-        std::cout << std::setw(12) << a[i - rank * n / commsize][j];
+      for (int j = 0; j < std::min(nrows, 6); j++) {
+        std::cout << std::setw(12) << a[i - rank * nrows][j];
       }
       std::cout << '\n';
     }
-    if (i - k * n / commsize == n / commsize - 1) {
+    if (i - k * nrows == nrows - 1) {
       k++;
     }
   }
@@ -64,7 +56,30 @@ void print_matrix(double **a) {
 // diagonalising matrix & getting inverse from connected matrix
 // transferring data between procs takes O(n) memspace
 // function zeroes k-th col except diagonal elem, choosing row's main elem
-int inverse_matrix(double **a, double **x, int k) {
+void inverse_matrix(double **a, double **x, int k, int lb) {
+  double local_max = 0.0;
+  int local_index = -1;
+  for (int i = 0; i < nrows; i++) {
+    int global_index = i + lb;
+    if (global_index >= k && std::fabs(a[i][k]) > local_max) {
+      local_max = std::fabs(a[i][k]);
+      local_index = global_index;
+    }
+  }
+
+  // Используем MPI_Reduce для определения глобального максимума и его индекса
+  struct {
+    double value;
+    int index;
+  } local_data = {local_max, local_index}, global_data;
+
+  MPI_Allreduce(&local_data, &global_data, 1, MPI_DOUBLE_INT, MPI_MAXLOC, MPI_COMM_WORLD);
+
+  if (global_data.value == 0.0) {
+    throw std::runtime_error(std::to_string(rank) + ": No inverse matrix\n");
+  }
+
+  /*
   // for transferring data between procs
   double *ms = new double[n];
   double *mf = new double[n];
@@ -72,8 +87,8 @@ int inverse_matrix(double **a, double **x, int k) {
   for (int i = 0; i < n; i++) {
     ms[i] = 0.0;
   }
-  for (int i = rank * n / commsize; i < (rank + 1) * n / commsize; i++) {
-    ms[i] = a[i - rank * n / commsize][k];
+  for (int i = rank * nrows; i < (rank + 1) * nrows; i++) {
+    ms[i] = a[i - rank * nrows][k];
   }
 
   // procs sum ms cols to mf col => mf = current col
@@ -94,14 +109,16 @@ int inverse_matrix(double **a, double **x, int k) {
   delete[] mf;
 
   // if main element is 0, matrix got no inverse
-  if (std::fabs(mf[M]) == 0) {
+  if (std::fabs(mf[M]) == 0.0) {
     return -1;
   }
+   */
 
   // t1 - proc with n-th row, n is diagonalised col number
   // t2 - proc with main element
-  int t1 = k / (n / commsize);
-  int t2 = M / (n / commsize);
+  int M = global_data.index;
+  int t1 = k / (nrows);
+  int t2 = M / (nrows);
 
   // new transferring data
   double *s1 = new double[4 * n];
@@ -113,16 +130,16 @@ int inverse_matrix(double **a, double **x, int k) {
   // proc with diagonal element puts needed rows to arrays
   if (rank == t1) {
     for (int i = n; i < 2 * n; i++) {
-      s1[i] = a[k - rank * n / commsize][i - n];
-      s1[i + 2 * n] = x[k - rank * n / commsize][i - n];
+      s1[i] = a[k - rank * nrows][i - n];
+      s1[i + 2 * n] = x[k - rank * nrows][i - n];
     }
   }
 
   // proc with main elem puts needed rows to arrays
   if (rank == t2) {
     for (int i = 0; i < n; i++) {
-      s1[i] = a[M - rank * n / commsize][i];
-      s1[i + 2 * n] = x[M - rank * n / commsize][i];
+      s1[i] = a[M - rank * nrows][i];
+      s1[i + 2 * n] = x[M - rank * nrows][i];
     }
   }
 
@@ -143,21 +160,21 @@ int inverse_matrix(double **a, double **x, int k) {
   // procs with main and diagonal elements swap rows
   if (rank == t2) {
     for (int i = n; i < 2 * n; i++) {
-      a[M - rank * n / commsize][i - n] = s2[i];
-      x[M - rank * n / commsize][i - n] = s2[i + 2 * n];
+      a[M - rank * nrows][i - n] = s2[i];
+      x[M - rank * nrows][i - n] = s2[i + 2 * n];
     }
   }
   if (rank == t1) {
     for (int i = 0; i < n; i++) {
-      a[k - rank * n / commsize][i] = s2[i];
-      x[k - rank * n / commsize][i] = s2[i + 2 * n];
+      a[k - rank * nrows][i] = s2[i];
+      x[k - rank * nrows][i] = s2[i + 2 * n];
     }
   }
 
   // all procs subtract diagonal row from their rows
   // (zeroing working col except diagonal elem)
-  for (int i = 0; i < n / commsize; i++) {
-    if (i + rank * n / commsize != k) {
+  for (int i = 0; i < nrows; i++) {
+    if (i + rank * nrows != k) {
       c = a[i][k];
       for (int j = 0; j < n; j++) {
         a[i][j] = a[i][j] - s2[j] * c;
@@ -167,7 +184,6 @@ int inverse_matrix(double **a, double **x, int k) {
   }
   delete[] s1;
   delete[] s2;
-  return 1;
 }
 
 int main(int argc, char **argv) {
@@ -183,13 +199,18 @@ int main(int argc, char **argv) {
   // each proc gets equal amount of rows
   int lb, ub;
   get_chunk(&lb, &ub);
-  int nrows = ub - lb + 1;
-  double *a = (double *)malloc(sizeof(*a) * nrows * n);
-  double *x = (double *)malloc(sizeof(*x) * nrows * n);
+  nrows = ub - lb + 1;
+
+  double **a = get_matrix();
+  double **x = new double*[nrows];
+  for (int i = 0; i < nrows; i++) {
+    x[i] = new double[n];
+  }
+
   // each proc fills its chunk of connected matrix
   for (int i = 0; i < nrows; i++) {
     for (int j = 0; j < n; j++) {
-      if (i + rank * n / commsize == j) {
+      if (i + rank * nrows == j) {
         x[i][j] = 1.0;
       } else {
         x[i][j] = 0.0;
@@ -197,32 +218,32 @@ int main(int argc, char **argv) {
     }
   }
 
-  get_matrix(a);
-
   MPI_Barrier(MPI_COMM_WORLD);
   double t = -MPI_Wtime();
 
   for (int i = 0; i < n; i++) { // find inverse matrix, diagonalise each col
-    int result = inverse_matrix(a, x, i);
-    if (result == -1) {
-      std::cout << "No inverse matrix\n";
-      for (int j = 0; j < n / commsize; j++) {
+    try {
+      inverse_matrix(a, x, i, lb);
+    }
+    catch(const std::exception &e) {
+      for (int j = 0; j < nrows; j++) {
         delete[] a[j];
         delete[] x[j];
       }
       delete[] a;
       delete[] x;
-      return -4;
+      std::cerr << "error: " << e.what() << '\n';
+      return 1;
     }
   }
 
   t += MPI_Wtime();
   if (rank == 0) {
-    std::cout << "commsize = " << commsize << ", n = " << n << ", t = " << t
-              << " sec\n";
+    std::cout << prefix << "commsize = " << commsize << ", n = " << n
+              << ", t = " << t << " sec\n";
   }
 
-  for (int i = 0; i < n / commsize; i++) {
+  for (int i = 0; i < nrows; i++) {
     delete[] a[i];
     delete[] x[i];
   }
