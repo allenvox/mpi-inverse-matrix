f335dbeb89c712ca94eb9b9461660dad9bbcba06
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2023-12-04 17:18:53 +0700

    Fix python plotting script

--- plot/main.py
+++ plot/main.py
@@ -14,9 +14,9 @@ def draw(filenames, labels, dest_filename):
     fig = plt.figure(figsize = (10 * cm, 7 * cm))
     ax = fig.add_subplot(111)
     ax.set_title("")
-    ax.set(xlabel = "Number of processes", ylabel = "Relative speedup")
+    ax.set(xlabel = "Number of processes", ylabel = "Speedup")
     ax.label_outer()
-    #ax.xaxis.set_ticks(np.arange(4, 16, 1))
+    ax.xaxis.set_ticks(np.arange(0, 17, 2))
     #ax.yaxis.set_ticks(np.arange(1.8, 9, 1))
     ax.xaxis.set_tick_params(direction='in', which='both')
     ax.yaxis.set_tick_params(direction='in', which='both')
@@ -24,10 +24,10 @@ def draw(filenames, labels, dest_filename):
         data = np.loadtxt(fname)
         x = data[:, 0]
         y = data[:, 1]
-        if datalabel == "N = 1000":
+        if datalabel == "N = 1500":
             marker = '-^'
             color = "orange"
-        elif datalabel == "N = 10000":
+        elif datalabel == "N = 5000":
             marker = '-s'
             color = "dodgerblue"
         else:
@@ -35,16 +35,18 @@ def draw(filenames, labels, dest_filename):
             color = "red"
         ax.plot(x, y, marker, c = color, markersize = 4.0, linewidth = 1.2, label = datalabel)
     labels = [item.get_text() for item in ax.get_xticklabels()]
-    labels[1] = '6\n(2x3)'
-    labels[2] = '8\n(2x4)'
-    labels[3] = '10\n(2x5)'
-    labels[4] = '12\n(2x6)'
-    labels[5] = '14\n(2x7)'
-    labels[6] = '16\n(2x8)'
+    labels[1] = '2\n(2x1)'
+    labels[2] = '4\n(2x2)'
+    labels[3] = '6\n(2x3)'
+    labels[4] = '8\n(2x4)'
+    labels[5] = '10\n(2x5)'
+    labels[6] = '12\n(2x6)'
+    labels[7] = '14\n(2x7)'
+    labels[8] = '16\n(2x8)'
     ax.set_xticklabels(labels)
     plt.tight_layout()
     ax.legend()
     fig.savefig(dest_filename, dpi = 300)
 
 if __name__ == "__main__":
-    draw(["gauss_1000.dat", "gauss_10000.dat", "linear.dat"], ["N = 1000", "N = 10000", "Linear speedup"], "out.png")
+    draw(["gauss_1500.dat", "gauss_5000.dat", "linear.dat"], ["N = 1500", "N = 5000", "Linear speedup"], "chart.png")
